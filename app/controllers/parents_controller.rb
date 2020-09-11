ad492296f3fa220c371df09a7a633ca5b62edbd1
HARAYUKA <tukituki442@icloud.com>
2020-09-11 23:17:29 +0900

    新規登録ページ完了

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -6,4 +6,20 @@ class ParentsController < ApplicationController
   def new
     @parent = Parent.new
   end
+
+  def create
+    @parent = Parent.new(parent_params)
+    if @parent.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @parent
+    else
+      render :new
+    end
+  end
+
+  private
+
+      def parent_params
+        params.require(:parent).permit(:name, :email, :password, :password_confirmation)
+      end
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,4 +6,20 @@ class TeachersController < ApplicationController
   def new
     @teacher = Teacher.new
   end
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @teacher
+    else
+      render :new
+    end
+  end
+
+  private
+
+      def teacher_params
+        params.require(:teacher).permit(:name, :email, :password, :password_confirmation)
+      end
 end
