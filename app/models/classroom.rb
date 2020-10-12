fc9538be47e963c137bf0f644f592b0de38918dc
HARAYUKA <tukituki442@icloud.com>
2020-10-13 01:25:24 +0900

    保護者一覧作成中

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,6 +1,8 @@
 class Classroom < ApplicationRecord
   # teacherとの親子関係
   has_many :teachers
+  accepts_nested_attributes_for :teachers
   # childrenとの親子関係
   has_many :children
+  accepts_nested_attributes_for :children
 end
