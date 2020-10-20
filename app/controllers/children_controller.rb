855ae44e43924d83c92ae7c46807cf4fb87a6117
HARAYUKA <tukituki442@icloud.com>
2020-10-20 23:58:24 +0900

    園児情報編集ルート設定でつまづき中

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -2,8 +2,6 @@ class ChildrenController < ApplicationController
   # 園児作成モーダル表示
   def new
     @parent = Parent.find(params[:parent_id])
-    # @classroom = Classroom.new
-    # @classroom.children.build
     @child = @parent.children.new
     @classrooms = Classroom.all
   end
@@ -11,21 +9,24 @@ class ChildrenController < ApplicationController
   # 園児作成モーダルでの園児追加
   def create
     @parent = Parent.find(params[:parent_id])
-    # if Classroom.create(class_params)
-    #   flash[:success] = '子供を追加しました。'
-    #   redirect_to @parent
-    # else
-    #   flash[:danger] = '登録できませんでした。'
-    #   render 'parents/show'
-    # end
     @child = @parent.children.new(child_params)
     if @child.save
       flash[:success] = '子供を追加しました。'
       redirect_to @parent
     else
-      flash[:danger] = '登録できませんでした。'
+      flash.now[:danger] = '登録できませんでした。'
       render 'parents/show'
     end
+  end
+
+  # 園児情報編集モーダル
+  def edit
+    @parent = Parent.find(params[:parent_id])
+    @child = @parent.children
+  end
+
+  # 園児情報編集モーダル更新
+  def update
   end
 
   private
@@ -33,9 +34,5 @@ class ChildrenController < ApplicationController
     def child_params
       params.require(:child).permit(:child_name, :age, :birthday, :gender, :classroom_id)
     end
-
-    # def class_params
-    #   params.require(:classroom).permit(:class_name, children_attributes: [:child_name, :age, :birthday, :gender])
-    # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,15 @@ Rails.application.routes.draw do
       get 'edit_manag_info' # 管理者が編集可能な情報
       patch 'update_manag_info' # 管理者が編集可能な情報の更新
     end
-    resources :children, only: :update do
-      collection do
-        get 'children/new' # 園児追加モーダル表示
-        post 'create' # 園児追加モーダル更新
-      end
+    resources :children do
+      # member do
+      #   get 'edit_child_info' # 園児情報編集モーダル表示
+      #   patch 'update_child_info' # 園児情報編集モーダル更新
+      # end
+      # collection do
+      #   get 'children/new' # 園児追加モーダル表示
+      #   post 'create' # 園児追加モーダル更新
+      # end
     end
   end
 
